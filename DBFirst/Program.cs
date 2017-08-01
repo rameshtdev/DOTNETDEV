e46f71be9a645790146a4db16e4a6d8cc5fcb6e5
Ramesh <Ramesh@CPS-TRN-L-T450>
2017-08-01 15:55:31 -0400

    DB First

---
+++ DBFirst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var dbcontext = new sampleDBEntities())
+            {
+                var assesssments = dbcontext.Assessments.OrderByDescending(o=>o.FirstName).ToList();
+
+                foreach(var item in assesssments)
+                {
+                    Console.WriteLine("FirstName:{0}, LastName:{1}",item.FirstName,item.LastName);
+                }
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
