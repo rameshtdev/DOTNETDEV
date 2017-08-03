9e11c07fe600fc5ef6980a4347eafe57e36a4258
Ramesh <Ramesh@CPS-TRN-L-T450>
2017-08-03 15:10:35 -0400

    Code First

---
+++ CodeFirst/Entities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Enrollment> enrollments { get; set; }
+    }
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Enrollment> enrollments { get; set; }
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+
+        public int CourseCourseId { get; set; }
+        public virtual Course course { get; set; }
+
+        public int StudentStudentId { get; set; }
+        public virtual Student student { get; set; }
+    }
+}
---
+++ CodeFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new CodeFirstDBContext())
+            {
+                var Student = new Student() { Name = "Paul" };
+                context.Students.Add(Student);
+                context.SaveChanges();
+            }
+            Console.ReadKey();
+        }
+    }
+}
