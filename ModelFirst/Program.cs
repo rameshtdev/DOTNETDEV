253888857e2da6c2611ef04fdd20076aef28d9f2
Ramesh <Ramesh@CPS-TRN-L-T450>
2017-08-04 15:50:33 -0400

    LINQ Queries

--- ModelFirst/Program.cs
+++ ModelFirst/Program.cs
@@ -15,6 +15,29 @@ namespace ModelFirst
                 var student = context.Students.Where(w => w.Name == "Mark").FirstOrDefault();
                 var courseList = context.Courses.Where(w=>w.Name!="Mech").ToList();
 
+                //Query Syntax
+                var studentsEnrolled = (from s in context.Students join e in context.Enrollments on s.StudentId equals e.StudentStudentId select s.Name).Distinct().ToList();
+                //Method syntax
+                var studentsEnrolled1 = context.Students.Join(context.Enrollments, s => s.StudentId, e => e.StudentStudentId, (s, e) => new { s.Name }).Distinct().ToList();
+
+                //Left Join
+                /*
+                    Select S.Name, COUNT(E.EnrollmentId) from Students S
+                    LEFT JOIN Enrollments E ON S.StudentId=E.StudentStudentId
+                    GROUP BY S.Name
+                 */
+                var LeftJoin = (from s in context.Students
+                                join e in context.Enrollments 
+                                on s.StudentId equals e.StudentStudentId
+                                group s by s.Name
+                                into es from e1 in es.DefaultIfEmpty()                               
+                                select new { es.Key, count = es.Count() }).Distinct().ToList();
+                foreach (var student1 in LeftJoin)
+                {
+                    Console.WriteLine("{0}-{1}",student1.Key,student1.count);
+
+                }
+                /*
                 foreach(var course in courseList)
                 {
                     var enroll = new Enrollment()
@@ -26,8 +49,10 @@ namespace ModelFirst
                     context.SaveChanges();
 
                 }
-                
+                */
             }
+
+            //
             Console.ReadKey();
         }
     }
