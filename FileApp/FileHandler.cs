68b8edaa72a97c5e622f27ef3e857adea05ef6f2
Ramesh <Ramesh@CPS-TRN-L-T450>
2017-08-21 15:39:38 -0400

    Stream reader and writer

---
+++ FileApp/FileHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileApp
+{
+    public class FileHandler
+    {
+        public void writeFile()
+        {
+            var path = @"C:\temp\CSharpFile.csv";
+            StreamWriter w;
+            if (!File.Exists(path))
+            {
+                w = new StreamWriter(path);
+                w.WriteLine(string.Format("{0},{1}", 1, "Mike"));
+            }
+            else
+            {
+                w = File.AppendText(path);
+                w.WriteLine(string.Format("{0},{1}", 2, "Mary"));
+            }
+            w.Close();
+        }
+        public void ReadFile()
+        {
+            var path = @"C:\temp\CSharpFile.csv";
+            StreamReader r = new StreamReader(path);
+            while(r.Peek()>0)
+            {
+                Console.WriteLine(r.ReadLine());
+            }
+            
+            r.Close();
+        }
+    }
+}
---
+++ FileApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHandler fileObj = new FileHandler();
+            fileObj.ReadFile();
+            Console.ReadKey();
+        }
+    }
+}
